a3bcd07cb472fc99fe13ebb522a42b24a9685b9a
Cracius <120117209+Cracius@users.noreply.github.com>
2024-10-27 21:12:59 +0300

    Перенос

---
+++ 5/script.js
@@ -0,0 +1,21 @@
+window.addEventListener("DOMContentLoaded", function ()
+{
+    const Things = {"pr1": 2500, "pr2": 3000, "pr3": 1000, "pr4": 1555, "pr5": 1333};
+    let amount = document.getElementById("amount");
+    let product = document.getElementById("goodsList");
+    let calcButton = document.getElementById("calculate");
+    let resultfield = document.getElementById("result");
+    calcButton.addEventListener("click", function ()
+    {
+        let inp = amount.value;
+        if (inp.match(/^\d+$/) !== null)
+        {
+            let res = Things[product.value] * inp;
+            resultfield.innerText = res;
+        }
+        else
+        {
+            resultfield.innerText = "Некорректный ввод количества";
+        }
+    });
+});
