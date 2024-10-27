a6ca5418472f2b200c7f8f01774ff9c039c0b713
Cracius <120117209+Cracius@users.noreply.github.com>
2024-10-27 21:18:05 +0300

    Update script.js

--- 5/script.js
+++ 5/script.js
@@ -2,7 +2,7 @@ window.addEventListener("DOMContentLoaded", function ()
 {
     const Things = {"pr1": 2500, "pr2": 3000, "pr3": 1000, "pr4": 1555, "pr5": 1333};
     let amount = document.getElementById("amount");
-    let product = document.getElementById("goodsList");
+    let product = document.getElementById("Things");
     let calcButton = document.getElementById("calculate");
     let resultfield = document.getElementById("result");
     calcButton.addEventListener("click", function ()
